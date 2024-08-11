64b126cb5086af47a85ce1014931b3af3bf6408f
Khanh Le <kle01@villanova.edu>
2024-08-11 20:36:04 +0700

    simple set up

---
+++ mongoose/schema/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: mongoose.Schema.Types.String,
+        required: [true, 'Email is required'],
+        unique: true,
+        validate: {
+            validator: (value) => validator.isEmail(value),
+            message: 'Email must be valid'
+        }
+    }
+})
