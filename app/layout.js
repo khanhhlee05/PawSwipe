2dfbacad504c1859049fef5935722d7eeca46a86
Mehdi Atmani <mehdi_atmani@brown.edu>
2024-08-19 11:42:32 -0500

    Vercel Analytics Added

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <Analytics />
+      </body>
     </html>
   );
 }
