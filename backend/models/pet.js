0148271180bea4c692212614c60bea1050a04c05
Christopher Magnuson <cmagnuson0489@gmail.com>
2024-08-24 13:37:59 -0700

    Initial card setup
    Initial backend setup for the pet cards. This is still subject to change

---
+++ backend/models/pet.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const petSchema = new mongoose.Schema({
+  name: String,
+  breed: String,
+  age: Number,
+  imageUrl: String,
+  description: String,
+  isAdopted: { type: Boolean, default: false },
+  addedDate: { type: Date, default: Date.now },
+});
+
+const Pet = mongoose.model('Pet', petSchema);
+
+module.exports = Pet;
