bc12786b4ad93e561f2259a135873558e7726ca8
Khanh Le <kle01@villanova.edu>
2024-08-28 19:54:01 -0400

    adoption schema added

---
+++ mongoose/schema/Adoption.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import { Pet } from "./Pet";
+import { Updock } from "next/font/google";
+
+const adoptionSchema = new mongoose.Schema({
+    adopterId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    petId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pet',
+        required: true
+    },
+    adoptionFee: {
+        type: mongoose.Schema.Types.Number
+        
+    },
+    adoptionDate: {
+        type: mongoose.Schema.Types.Date,
+        default: Date.now()
+    },
+    status: {
+        type: mongoose.Schema.Types.String,
+        enum: ['pending', 'approved', 'rejected'],
+        default: 'pending'
+    },
+    note: {
+        type: mongoose.Schema.Types.String,
+        maxlength: 200
+    },
+    createdAt: {
+        type: mongoose.Schema.Types.Date,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: mongoose.Schema.Types.Date,
+        default: Date.now()
+    }
+})
+
+adoptionSchema.pre('save', async function (next) {
+    this.updatedAt = Date.now()
+    next()
+})
+
+export const Adoption = mongoose.model('Adoption', adoptionSchema) || mongoose.models.Adoption
