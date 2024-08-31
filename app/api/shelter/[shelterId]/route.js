3514f703aaaf56d8f55fd527d4466b165c190ba9
Khanh Le <kle01@villanova.edu>
2024-08-31 12:41:11 -0400

    done CRUD API for shelter

--- app/api/shelter/[shelterId]/route.js
+++ app/api/shelter/[shelterId]/route.js
@@ -4,7 +4,26 @@ import { NextResponse } from "next/server";
 
 
 //fetch a specific shelter
-export async function GET(req, {params}){
-    const {shelterId} = params
-    
+export async function GET(req, { params }) {
+    try {
+        const { shelterId } = params
+
+        const myShelter = await Shelter.findById(shelterId)
+
+        if (!myShelter) {
+            return new NextResponse("Shelter not found", { status: 404 })
+
+        }
+
+        return new NextResponse(JSON.stringify(myShelter), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+    } catch (error) {
+        return new NextResponse(error, { status: 500 })
+    }
+
 }
