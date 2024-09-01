d93864da3f1a9796de6437270231bf828d08042f
Khanh Le <kle01@villanova.edu>
2024-09-01 17:56:39 -0400

    add connection in the review

---
+++ app/api/adoption/route.js
@@ -0,0 +1,11 @@
+import { Adoption } from "@/mongoose/schema/Adoption";
+import { NextResponse } from "next/server";
+
+//Get a list of adoptions in the databse
+export async function GET(req){
+    try {
+        await
+    } catch (error) {
+        
+    }
+}
--- app/api/review/[Id]/route.js
+++ app/api/review/[Id]/route.js
@@ -1,11 +1,13 @@
 import { Review } from "@/mongoose/schema/Review";
 import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongodb";
 
 
 
 //Delete a review based on a review Id
 export async function DELETE(req, {params}){
     try {
+        await dbConnect()
         const {Id} = params
         const deleteReview = await Review.findByIdAndDelete(Id)
 
@@ -23,6 +25,7 @@ export async function DELETE(req, {params}){
 
 export async function GET(req, {params}){
     try {
+        await dbConnect()
         const {Id} = params
         const shelterReview = await Review.find({shelter: Id})
 
