0db6d9c308be4d3ce930fa28d4f0dd7045ce17ff
Chris <leework054@gmail.com>
2024-09-02 10:15:13 -0400

    working on registering user credentials

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,9 +1,15 @@
 import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongodb";
+import mongoose from "mongoose";
 
 export async function POST(request: Request) {
   try {
     const { email, password } = await request.json();
+    const bcrypt = require("bcrypt")
+    const hashedPassword = await bcrypt.hash(password, 10)
+    await dbConnect();
 
+    const createAccount = 
     console.log({ email, password });
   } catch (error: any) {
     console.log(error.message);
