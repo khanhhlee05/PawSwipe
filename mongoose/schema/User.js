d812fc6857e24c7edcdc6bcf843fa1974e5c858d
Khanh Le <kle01@villanova.edu>
2024-09-03 13:50:21 -0400

    add login compare function

--- mongoose/schema/User.js
+++ mongoose/schema/User.js
@@ -110,4 +110,17 @@ userSchema.pre("save", function (next) {
   next();
 });
 
+userSchema.statics.login = async function (email, password){
+  const user = await this.findOne({email})
+  if (user){
+      const auth = await bcrypt.compare(password, user.password)
+      if (auth) {
+          return user;
+      }
+      throw Error("Incorrect Password")
+  }
+  throw Error("Incorrect Email")
+}
+
+
 export const User = mongoose.models.User || mongoose.model("User", userSchema);
